a745eb3576f71cfd9652f8141287354d7657140e
ch1nru5t <chinrust@gmail.com>
2021-12-26 01:40:34 +0530

    Hello World

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+/*
+* keywords - specific meaning for the language (public, class)
+* public - access modifier, allows us to define the scope or how other parts of your code or some one else's code can access this code
+* class - used to define a class followed by the name of the class
+* {} - define the body of the class
+* static - keyword, we need to have main method static for java to find the starting point of our code
+* void - the method will not be returning anything
+* (parameters)
+* {} - code block, mandatory in a method declaration
+* */
