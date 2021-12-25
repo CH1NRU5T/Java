b0e89394328d680c14fcff160f27c0025b70c705
ch1nru5t <chinrust@gmail.com>
2021-12-26 01:54:27 +0530

    .

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,15 +1,24 @@
 public class Hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
     }
 }
 /*
-* keywords - specific meaning for the language (public, class)
-* public - access modifier, allows us to define the scope or how other parts of your code or some one else's code can access this code
-* class - used to define a class followed by the name of the class
-* {} - define the body of the class
-* static - keyword, we need to have main method static for java to find the starting point of our code
-* void - the method will not be returning anything
-* (parameters)
-* {} - code block, mandatory in a method declaration
-* */
+ * keywords - specific meaning for the language (public, class)
+ * public - access modifier, allows us to define the scope or how other parts of your code or some one else's code can access this code
+ * class - used to define a class followed by the name of the class
+ * {} - define the body of the class
+ * static - keyword, we need to have main method static for java to find the starting point of our code
+ * void - the method will not be returning anything
+ * (parameters)
+ * {} - code block, mandatory in a method declaration
+ *  int - data type
+ *  myFirstNumber - name of the variable
+ *  = - assignment, initialisation
+ *  Expression - This is a construct that evaluates to a single value
+ *  "stringLiteral" - Any sequence of characters  surrounded by double quotes is a String literal in Java. It's value cannot be changed unlike a variable
+ *  operators - +,* are the operators in Java
+ * */
